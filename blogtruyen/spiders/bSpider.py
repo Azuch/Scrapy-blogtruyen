2ceff9079032216db5a392ad8e7d10d9197512f9
Azuch <50861752+Azuch@users.noreply.github.com>
2024-05-17 13:13:45 +0700

    Update bSpider.py
    change back to blogtruyen

--- blogtruyen/spiders/bSpider.py
+++ blogtruyen/spiders/bSpider.py
@@ -1,41 +1,42 @@
 import scrapy
 from urllib.parse import urljoin
 from scrapy.linkextractors import LinkExtractor
-
+import os
 
 class BspiderSpider(scrapy.Spider):
     name = "bSpider"
-    baseUrl = "https://nettruyencc.com"
-    
+    base = "https://m.blogtruyen.vn" 
     def start_requests(self):
         with open("links", "r") as f:
             for url in f:
                 yield scrapy.Request(url.strip(), callback=self.parse)
 
     def parse(self, response):
-        chapters_links = response.xpath('//div[contains(@class, "chapter")]//a[starts-with(@href, "https")]/@href').extract()
+        base = "https://m.blogtruyen.vn"
+        chapters_links = response.xpath("//article[@class='list-chapter']//a/@href").extract()
         manga_name = response.url.split("/")[-1]
+        chapters_links.reverse()
         for index, chapter_link in enumerate(chapters_links):
-            yield scrapy.Request(chapter_link, callback=self.parse_chapter, meta={"manga_name": manga_name, "chapter_name": f"chapter-{index}"})
+            yield scrapy.Request(url=urljoin(base, chapter_link), callback=self.parse_chapter, meta={"manga_name": manga_name, "chapter_name": f"chapter-{index}"})
 
     def parse_chapter(self, response):
         chapter_name = response.meta["chapter_name"]
         manga_name = response.meta["manga_name"]
-        # Extract image URLs
-        img_tags = response.xpath('//*[@class="page-chapter"]//img')
 
-        for idx, img_tag in enumerate(img_tags):
-            img_urls = img_tag.xpath('./@*[contains(., "https")]').extract()
-            for idx_url, img_url in enumerate(img_urls):
-                img_name = f'image-{idx}-{idx_url}.jpg'
-                img_path = f'manga/{manga_name}/{chapter_name}/{img_name}'
-                yield {"chapter_name": chapter_name, "manga_name": manga_name, "img_url": img_url}
-                yield scrapy.Request(url=img_url, meta={'img_path': img_path, 'referer': response.url}, callback=self.save_img)
+        # Extract image URLs
+        imgs = response.xpath("//article[@class='chapter-detail']//img/@src").extract()
+        for idx, img_url in enumerate(imgs):
+            img_name = f'image-{idx}.jpg'
+            img_dir = f"manga/{manga_name}/{chapter_name}"
+            try:
+                os.makedirs(img_dir, exist_ok=True)
+            except Exception as e:
+                print(f"An error occurred: {e}")
+            img_path = f'manga/{manga_name}/{chapter_name}/{img_name}'
+            yield scrapy.Request(url=img_url, meta={'img_path': img_path, 'referer': response.url}, callback=self.save_img)
 
     def save_img(self, response):
         img_path = response.meta['img_path']
         with open(img_path, 'wb') as f:
             f.write(response.body)
         self.log(f'Saved image {img_path}')
-
-
